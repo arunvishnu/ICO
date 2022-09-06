47fa4f35a14fc044e5f0911890271a1133576697
Arun Vishnu <arun.vishnu@gmail.com>
2022-09-06 13:34:34 -0400

    code added

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x347c85001e836F2c538c6F01421E2e514ff16548";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
